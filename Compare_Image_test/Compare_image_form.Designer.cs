ef581780bd8664895233a0ed06549a90e0336446
lienism <didwhdqls7@gmail.com>
2023-01-07 13:00:40 +0900

    first

---
+++ Compare_Image_test/Compare_image_form.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Compare_Image_test
+{
+    partial class Compare_image_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Left_pictureBox = new System.Windows.Forms.PictureBox();
+            this.Right_pictureBox = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Compare_process = new System.Windows.Forms.Button();
+            this.Left_pictureButton = new System.Windows.Forms.Button();
+            this.Right_picture_Button = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.hist_compare_res = new System.Windows.Forms.Label();
+            this.hist_compare_value_res = new System.Windows.Forms.Label();
+            this.Match_Shape_value_res = new System.Windows.Forms.Label();
+            this.Match_Shape_res = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Left_pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Right_pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Left_pictureBox
+            // 
+            this.Left_pictureBox.Location = new System.Drawing.Point(12, 22);
+            this.Left_pictureBox.Name = "Left_pictureBox";
+            this.Left_pictureBox.Size = new System.Drawing.Size(305, 270);
+            this.Left_pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Left_pictureBox.TabIndex = 0;
+            this.Left_pictureBox.TabStop = false;
+            // 
+            // Right_pictureBox
+            // 
+            this.Right_pictureBox.Location = new System.Drawing.Point(440, 22);
+            this.Right_pictureBox.Name = "Right_pictureBox";
+            this.Right_pictureBox.Size = new System.Drawing.Size(305, 270);
+            this.Right_pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Right_pictureBox.TabIndex = 1;
+            this.Right_pictureBox.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Compare_process
+            // 
+            this.Compare_process.Location = new System.Drawing.Point(345, 356);
+            this.Compare_process.Name = "Compare_process";
+            this.Compare_process.Size = new System.Drawing.Size(75, 23);
+            this.Compare_process.TabIndex = 2;
+            this.Compare_process.Text = "Compare";
+            this.Compare_process.UseVisualStyleBackColor = true;
+            this.Compare_process.Click += new System.EventHandler(this.Compare_process_Click);
+            // 
+            // Left_pictureButton
+            // 
+            this.Left_pictureButton.Location = new System.Drawing.Point(92, 314);
+            this.Left_pictureButton.Name = "Left_pictureButton";
+            this.Left_pictureButton.Size = new System.Drawing.Size(130, 23);
+            this.Left_pictureButton.TabIndex = 5;
+            this.Left_pictureButton.Text = "Left_pictureButton";
+            this.Left_pictureButton.UseVisualStyleBackColor = true;
+            this.Left_pictureButton.Click += new System.EventHandler(this.Left_pictureButton_Click);
+            // 
+            // Right_picture_Button
+            // 
+            this.Right_picture_Button.Location = new System.Drawing.Point(521, 314);
+            this.Right_picture_Button.Name = "Right_picture_Button";
+            this.Right_picture_Button.Size = new System.Drawing.Size(158, 23);
+            this.Right_picture_Button.TabIndex = 6;
+            this.Right_picture_Button.Text = "Right_pictureButton";
+            this.Right_picture_Button.UseVisualStyleBackColor = true;
+            this.Right_picture_Button.Click += new System.EventHandler(this.Right_picture_Button_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // hist_compare_res
+            // 
+            this.hist_compare_res.AutoSize = true;
+            this.hist_compare_res.Location = new System.Drawing.Point(228, 386);
+            this.hist_compare_res.Name = "hist_compare_res";
+            this.hist_compare_res.Size = new System.Drawing.Size(101, 12);
+            this.hist_compare_res.TabIndex = 7;
+            this.hist_compare_res.Text = "hist_compare_res : ";
+            // 
+            // hist_compare_value_res
+            // 
+            this.hist_compare_value_res.AutoSize = true;
+            this.hist_compare_value_res.Location = new System.Drawing.Point(335, 386);
+            this.hist_compare_value_res.Name = "hist_compare_value_res";
+            this.hist_compare_value_res.Size = new System.Drawing.Size(35, 12);
+            this.hist_compare_value_res.TabIndex = 8;
+            this.hist_compare_value_res.Text = "label2";
+            // 
+            // Match_Shape_value_res
+            // 
+            this.Match_Shape_value_res.AutoSize = true;
+            this.Match_Shape_value_res.Location = new System.Drawing.Point(335, 418);
+            this.Match_Shape_value_res.Name = "Match_Shape_value_res";
+            this.Match_Shape_value_res.Size = new System.Drawing.Size(35, 12);
+            this.Match_Shape_value_res.TabIndex = 10;
+            this.Match_Shape_value_res.Text = "label3";
+            // 
+            // Match_Shape_res
+            // 
+            this.Match_Shape_res.AutoSize = true;
+            this.Match_Shape_res.Location = new System.Drawing.Point(228, 418);
+            this.Match_Shape_res.Name = "Match_Shape_res";
+            this.Match_Shape_res.Size = new System.Drawing.Size(101, 12);
+            this.Match_Shape_res.TabIndex = 9;
+            this.Match_Shape_res.Text = "Match_Shape_res : ";
+            // 
+            // Compare_image_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Match_Shape_value_res);
+            this.Controls.Add(this.Match_Shape_res);
+            this.Controls.Add(this.hist_compare_value_res);
+            this.Controls.Add(this.hist_compare_res);
+            this.Controls.Add(this.Right_picture_Button);
+            this.Controls.Add(this.Left_pictureButton);
+            this.Controls.Add(this.Compare_process);
+            this.Controls.Add(this.Right_pictureBox);
+            this.Controls.Add(this.Left_pictureBox);
+            this.Name = "Compare_image_form";
+            this.Text = "Compare_image_form";
+            ((System.ComponentModel.ISupportInitialize)(this.Left_pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Right_pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Left_pictureBox;
+        private System.Windows.Forms.PictureBox Right_pictureBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Compare_process;
+        private System.Windows.Forms.Button Left_pictureButton;
+        private System.Windows.Forms.Button Right_picture_Button;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Label hist_compare_res;
+        private System.Windows.Forms.Label hist_compare_value_res;
+        private System.Windows.Forms.Label Match_Shape_value_res;
+        private System.Windows.Forms.Label Match_Shape_res;
+    }
+}
+
---
+++ Compare_Image_test/Compare_image_form.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace Compare_Image_test
+{
+    public partial class Compare_image_form : Form
+    {
+        public Compare_image_form()
+        {
+            InitializeComponent();
+        }
+        Mat Left_Mat = new Mat();
+        Mat Right_Mat = new Mat();
+
+        private void Left_pictureButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != "")
+            {
+                Left_Mat = Cv2.ImRead(openFileDialog1.FileName);
+                Left_pictureBox.Image = new Bitmap(openFileDialog1.FileName);
+            }
+        }
+
+        private void Right_picture_Button_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+            if (openFileDialog2.FileName != "")
+            {
+                Right_Mat = Cv2.ImRead(openFileDialog2.FileName);
+                Right_pictureBox.Image = new Bitmap(openFileDialog2.FileName);
+            }
+        }
+
+        private void Compare_process_Click(object sender, EventArgs e)
+        {
+            Mat Left_RGB_processed = RGB_Process(Left_Mat);
+            Mat Right_RGB_processed = RGB_Process(Right_Mat);
+            Double[] Result = Compare_Image(Left_RGB_processed, Right_RGB_processed);
+            hist_compare_value_res.Text = "" + Result[0];
+            Match_Shape_value_res.Text = "" + Result[1];
+            Debug.WriteLine("asdf");
+        }
+        public Double[] Compare_Image(Mat left , Mat right)
+        {
+            Double[] Result = new double[2];
+            Double Match_Shape_res = 0.0;
+            Double histogram_compare_res = 0.0;
+            OpenCvSharp.Point[][] left_contour;
+            OpenCvSharp.Point[][] right_contour;
+            HierarchyIndex[] left_hi;
+            HierarchyIndex[] right_hi;
+
+            left.ConvertTo(left, MatType.CV_8U);
+            right.ConvertTo(right, MatType.CV_8U);
+            int left_channelNum = left.Channels();
+            int right_channelNum = right.Channels();
+            var left_histogram = new Mat[left_channelNum];
+            var right_histogram = new Mat[right_channelNum];
+
+            int[] hdims = { 256 };
+            Rangef[] ranges = { new Rangef(0, 256) };
+
+            left_histogram[0] = new Mat();
+            right_histogram[0] = new Mat();
+
+
+            Mat template_compare_res = new Mat();
+            
+            Cv2.CvtColor(left, left, ColorConversionCodes.BGR2GRAY);
+            Cv2.CvtColor(right, right, ColorConversionCodes.BGR2GRAY);
+            Cv2.FindContours(left, out left_contour, out left_hi, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+            Cv2.FindContours(right, out right_contour, out right_hi, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+            if(left_contour.Length == 1 && right_contour.Length == 1)
+            {
+                Match_Shape_res = Cv2.MatchShapes(left_contour[0], right_contour[0], ShapeMatchModes.I1);
+                Cv2.CalcHist(
+                new Mat[] { left },
+                new int[] { 0 },
+                null,
+                left_histogram[0],
+                1,
+                hdims,
+                ranges);
+                Cv2.CalcHist(
+                new Mat[] { right },
+                new int[] { 0 },
+                null,
+                right_histogram[0],
+                1,
+                hdims,
+                ranges);
+                histogram_compare_res = Cv2.CompareHist(left_histogram[0], right_histogram[0], HistCompMethods.Bhattacharyya);
+                Result[0] = histogram_compare_res;
+                Result[1] = Match_Shape_res;
+            }
+            //Cv2.MatchTemplate(left, right, template_compare_res, TemplateMatchModes.CCoeffNormed);
+           
+
+            return Result;
+        }
+        public Mat RGB_Process(Mat src)
+        {
+            Mat result = new Mat();
+            Mat Dummy_mat = new Mat();
+            OpenCvSharp.Point[][] Blue;
+            OpenCvSharp.Point[][] Green;
+            OpenCvSharp.Point[][] Red;
+            OpenCvSharp.Point[][] Dummy;
+            OpenCvSharp.Point[][] result_contour;
+            HierarchyIndex[] result_hierachy;
+            HierarchyIndex[] blue_hi;
+            HierarchyIndex[] green_hi;
+            HierarchyIndex[] red_hi;
+            Mat[] BGR;
+            BGR = Cv2.Split(src);
+            for (int i = 0; i < BGR.Length; i++)
+            {
+                Cv2.CvtColor(BGR[i], BGR[i], ColorConversionCodes.GRAY2BGR);
+                Cv2.CvtColor(BGR[i], BGR[i], ColorConversionCodes.BGR2GRAY);
+                Cv2.Threshold(BGR[i], BGR[i], 0, 255, ThresholdTypes.Otsu | ThresholdTypes.Binary);
+                for (int a = 0; a <= 6; a++)
+                {
+                    Cv2.Erode(BGR[i], BGR[i], new Mat());
+                    Cv2.Dilate(BGR[i], BGR[i], new Mat());
+                }
+            }
+            Cv2.FindContours(BGR[0], out Blue, out blue_hi, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+            Cv2.FindContours(BGR[1], out Green, out green_hi, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+            Cv2.FindContours(BGR[2], out Red, out red_hi, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+            double Blue_sum = 0;
+            for (int i = 0; i < Blue.Length; i++)
+            {
+                Blue_sum = Blue_sum + Cv2.ContourArea(Blue[i]);
+            }
+            double Green_sum = 0;
+            for (int i = 0; i < Green.Length; i++)
+            {
+                Green_sum = Green_sum + Cv2.ContourArea(Green[i]);
+            }
+            double Red_sum = 0;
+            for (int i = 0; i < Red.Length; i++)
+            {
+                Red_sum = Red_sum + Cv2.ContourArea(Red[i]);
+            }
+            if (Blue_sum < Red_sum)
+            {
+                Dummy = Blue;
+                Dummy_mat = BGR[0];
+                Blue = Red;
+                BGR[0] = BGR[2];
+                Red = Dummy;
+                BGR[2] = Dummy_mat;
+            }
+            if (Blue_sum < Green_sum)
+            {
+                Dummy = Blue;
+                Dummy_mat = BGR[0];
+                Blue = Green;
+                Dummy_mat = BGR[1];
+                Green = Dummy;
+                BGR[1] = Dummy_mat;
+            }
+            if (Red_sum < Green_sum)
+            {
+                Dummy = Green;
+                Dummy_mat = BGR[1];
+                Green = Red;
+                BGR[1] = BGR[2];
+                Red = Dummy;
+                BGR[1] = Dummy_mat;
+            }
+            Cv2.BitwiseAnd(BGR[0], BGR[1], result);
+            Cv2.BitwiseOr(result, BGR[2], result);
+            Cv2.FindContours(result, out result_contour, out result_hierachy, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+            Cv2.BitwiseAnd(result.CvtColor(ColorConversionCodes.GRAY2BGR), src, result);
+            return result;
+        }
+    }
+}
